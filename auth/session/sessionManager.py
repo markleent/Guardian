7de726303b49e34397cbae91eb577ecd34daee0e
Ken <ashes.dust@gmail.com>
2014-02-05 16:57:20 +0400

    Removed useless logger...

--- auth/session/sessionManager.py
+++ auth/session/sessionManager.py
@@ -1,27 +1,8 @@
 import sys
 import auth.config
-import logging
-
-FORMAT = '%(asctime)-15s %(message)s'
-logging.basicConfig(format=FORMAT)
-
-try:
-    import auth.session.dictAdapter as dictAdapter
-except ImportError:
-    if auth.config.DEBUG:
-        logging.warning('Import Error: %s', 'Null Session (dictAdapter) could not be imported, please check if you need this')
-
-try:
-    import auth.session.flaskAdapter as flaskAdapter
-except ImportError:
-    if config.DEBUG:
-        logging.warning('Import Error: %s', 'flask Session (flaskAdapter) could not be imported, please check if you need this')
-
-try:
-    import auth.session.tornadoAdapter as tornadoAdapter
-except ImportError:
-    if auth.auth.config.DEBUG:
-        logging.warning('Import Error: %s', 'tornado Session (tornadoAdapter) could not be imported, please check if you need this')
+import auth.session.dictAdapter as dictAdapter
+import auth.session.flaskAdapter as flaskAdapter
+import auth.session.tornadoAdapter as tornadoAdapter
 
 
 ADAPTERS = {
